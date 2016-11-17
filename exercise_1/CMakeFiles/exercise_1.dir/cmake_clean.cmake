5d2cfc5431325a4ebbf2e116513bc5ad4abe7718
Mateusz Muszak <mateusz.muszak.ext@nokia.com>
2016-11-17 13:56:23 +0100

    Second commit to repository

---
+++ exercise_1/CMakeFiles/exercise_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exercise_1.dir/main.cpp.o"
+  "exercise_1.pdb"
+  "exercise_1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exercise_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
